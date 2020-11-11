02dec902be97637a3ad35263379348522fd2e535
SubspaceBob <robert.gothall@gmail.com>
2020-11-11 16:09:34 +0100

    Threads (#9)
    * first multithreaded example with stubbed methods and updated CMakeLists
    
    * added gitignore file
    
    * Shared memory without actual reading writing
    
    * emulator-main-threads and inputhandler_main is working together with GearReq and quit-button
    
    * some cleaning after review

---
+++ Emulator-main-threads.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <thread>
+#include "CanInput.h"
+#include "engine.h"
+#include "GearClass.h"
+#include "frameToBus.h"
+#include <atomic>
+
+std::atomic<bool> something;
+void runCANIO(SharedMemory *memory, CANReader *canReader)
+{
+    
+    while(true) {
+        bool terminate = canReader->ReadCANWriteToMemory(memory);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        //FramePacker(Engine, Gearbox);
+        if (something) {
+            
+            break;
+        }
+    }
+}
+
+void runVehicle(SharedMemory *memory){
+    while(true) {
+        canInput from_memory = memory->read_memory();
+        std::cout << "Gear Request: " << (int)from_memory.GearReq << std::endl << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        //    Engine.run();
+        //    Gearbox.run();
+        if (from_memory.Ignition==1)
+        {
+            something.exchange(true);
+            break;
+        }
+    }
+}
+
+
+int main(){
+    CANReader canReader;
+    SharedMemory memory;
+    Engine Engine;
+    Gearbox Gearbox;
+    something = false;
+
+    canReader.start_can();
+    std::thread t3(runCANIO, &memory, &canReader);
+    std::thread t4(runVehicle, &memory);
+
+    t3.join();
+    t4.join();
+    return 0;
+}
+
+
+    
+/*
+class EmuMainThreads{
+public:
+    EmuMainThreads() = default;
+    ~EmuMainThreads(){};
+    canInput inputVal;
+    Engine Engine;
+    Gearbox Gearbox;
+    bool error=false;
+
+    void runCANReader();
+    void runVehicle();
+};
+
+void EmuMainThreads::runCANReader(){
+}
+
+void EmuMainThreads::runVehicle(){
+}*/
+
