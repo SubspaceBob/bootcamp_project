65a7f873862f9b74e2f1ebbd762dcdbab0bfa36d
GreenJessica <jessica.green.enelunden@gmail.com>
2020-11-20 13:53:41 +0100

    Added a testcase that will fail in order to show reds in HTML

--- Code/2_Emulator/Test/gearbox_test.cpp
+++ Code/2_Emulator/Test/gearbox_test.cpp
@@ -13,6 +13,21 @@ TEST(gearboxTest, constructor) {
     ASSERT_EQ((int)gearbox.getGearStick() , 0);
     ASSERT_EQ(gearbox.getRPS() , 0);
 }
+//__Test_of_constructor___________________________________
+//__DEMO_Test_designed_to_fail____________________________
+
+TEST(gearboxTest, Demo_thisShouldFail) {
+    // Set up test
+    Gearbox gearbox;
+
+    // Do checks
+    ASSERT_EQ(gearbox.getEngagedGear(), 0);
+    // TODO: Overload =operator in order to 
+    // compare gearpattern instead of int
+    //This will fail to show red in test report
+    EXPECT_EQ((int)gearbox.getGearStick() , 1); 
+    EXPECT_EQ(gearbox.getRPS() , 0);
+}
 
 //__Test_Main_____________________________________________
 int main(int argc, char **argv) {
